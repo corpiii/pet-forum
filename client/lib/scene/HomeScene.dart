bdbe4ba92c16e94a46cf9be01aa3c1421f2a0586
corpi <lj845854@gmail.com>
2024-05-28 22:27:03 +0900

    feat: client. HomeScene 생성

---
+++ client/lib/scene/HomeScene.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeScene extends StatelessWidget {
+  const HomeScene({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.amberAccent,
+      ),
+    );
+  }
+}
