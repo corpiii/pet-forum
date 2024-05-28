a82f3c7b87ee78078bdf796ded16cdb44ea718a5
corpi <lj845854@gmail.com>
2024-05-28 22:13:33 +0900

    first commit

---
+++ client/android/app/src/main/kotlin/com/corpi/mongNyang_app/mong_nyang/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.corpi.mongNyang_app.mong_nyang
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ server/src/main/java/com/corpi/mongNyang/MongNyangApplication.java
@@ -0,0 +1,13 @@
+package com.corpi.mongNyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongNyangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongNyangApplication.class, args);
+	}
+
+}
