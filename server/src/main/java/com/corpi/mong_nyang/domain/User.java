98a7a21f04b279fdd38935d771a7331181a7ede8
corpi <lj845854@gmail.com>
2024-05-29 14:14:40 +0900

    feat: server. user 엔티티 생성

---
+++ server/src/main/java/com/corpi/mong_nyang/domain/User.java
@@ -0,0 +1,27 @@
+package com.corpi.mong_nyang.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+public class User {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "user_id")
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private LocalDateTime created_at;
+}
