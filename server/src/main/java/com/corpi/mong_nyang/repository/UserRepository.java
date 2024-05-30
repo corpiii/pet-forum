130630b2ebf5902d3d38fa4051e5ae773e5737db
corpi <lj845854@gmail.com>
2024-05-30 18:33:24 +0900

    feat: server. UserService 내 findOne 메서드 생성 및 Repository에 Email을 통한 검색 구현

--- server/src/main/java/com/corpi/mong_nyang/repository/UserRepository.java
+++ server/src/main/java/com/corpi/mong_nyang/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.corpi.mong_nyang.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
 }
--- server/src/main/java/com/corpi/mong_nyang/service/UserService.java
+++ server/src/main/java/com/corpi/mong_nyang/service/UserService.java
@@ -2,17 +2,27 @@ package com.corpi.mong_nyang.service;
 
 import com.corpi.mong_nyang.domain.User;
 import com.corpi.mong_nyang.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class UserService {
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     public Long join(String name, String email, String password) {
         User user = User.of(name, email, password);
 
+        // validation
+
         userRepository.save(user);
 
         return user.getId();
+    }
+
+    public User findOne(String email) {
+        return userRepository.findByEmail(email);
     }
 }
