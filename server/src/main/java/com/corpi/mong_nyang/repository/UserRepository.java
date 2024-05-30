652f8ecfa61848dc098bd4b744b6d9c576cbce41
corpi <lj845854@gmail.com>
2024-05-30 18:23:13 +0900

    feat: server. UserRepository JPA인터페이스 생성

---
+++ server/src/main/java/com/corpi/mong_nyang/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.corpi.mong_nyang.repository;
+
+import com.corpi.mong_nyang.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
