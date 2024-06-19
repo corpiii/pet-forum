e512d940eac81ae814206eccda7a929f24e9e76a
corpi <lj845854@gmail.com>
2024-06-19 20:09:08 +0900

    feat: server. 댓글 수정 기능 구현

--- server/src/main/java/com/corpi/mong_nyang/controller/HelpPostCommentController.java
+++ server/src/main/java/com/corpi/mong_nyang/controller/HelpPostCommentController.java
@@ -76,6 +76,28 @@ public class HelpPostCommentController {
         return ResponseEntity.ok("대댓글이 달렸습니다.");
     }
 
-    // 대댓글 테스트
-    // 수정 삭제
+    @PutMapping("/reply/{commentId}")
+    public ResponseEntity<?> updateComment(@PathVariable("commentId") long id, @RequestHeader("Authorization") String token, @RequestBody HelpPostCommentRequest request) throws JsonProcessingException {
+        // 토큰 확인
+        if (!jwtTokenUtil.isValidToken(token, false)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Access 토큰이 유효하지 않습니다.");
+        }
+
+        HelpPostComments foundedComment = helpPostCommentService.findById(id);
+
+        if (foundedComment == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("comment가 존재하지 않습니다.");
+        }
+
+        String email = jwtTokenUtil.getUserEmail(token);
+        User author = userService.findOne(email);
+
+        if (foundedComment.getAuthor().equals(author)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("권한이 없습니다.");
+        }
+
+        foundedComment.changeContent(request.getContent());
+
+        return ResponseEntity.ok("성공적으로 댓글이 수정되었습니다.");
+    }
 }
